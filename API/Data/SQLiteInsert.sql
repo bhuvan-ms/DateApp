4109c26a128a8f9e690781a4288669edcfd35852
Bhuvan <bhuvanms31@gmail.com>
2023-12-25 09:27:02 +0530

    first commit

---
+++ API/Data/SQLiteInsert.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "User 1");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "User 2");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "User 3");
