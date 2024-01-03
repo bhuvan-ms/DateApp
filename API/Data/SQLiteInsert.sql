35fb43fa1145231ad75f11e4871fcb08deccd59b
Bhuvan <bhuvanms31@gmail.com>
2024-01-03 23:58:05 +0530

    add register, login endpoint and DTOs

--- API/Data/SQLiteInsert.sql
+++ API/Data/SQLiteInsert.sql
@@ -1,9 +1,11 @@
 -- SQLite
-INSERT INTO Users (Id, UserName)
-VALUES (1, "User 1");
+-- INSERT INTO Users (Id, UserName)
+-- VALUES (1, "User 1");
 
-INSERT INTO Users (Id, UserName)
-VALUES (2, "User 2");
+-- INSERT INTO Users (Id, UserName)
+-- VALUES (2, "User 2");
 
-INSERT INTO Users (Id, UserName)
-VALUES (3, "User 3");
+-- INSERT INTO Users (Id, UserName)
+-- VALUES (3, "User 3");
+
+SELECT * FROM Users
